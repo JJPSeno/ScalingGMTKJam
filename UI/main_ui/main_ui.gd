8414de168ff6c7d21528f48b24e0295a4a95e777
Accidentallyc <project.cassey@gmail.com>
2024-08-18 19:16:17 +1200

    Aligned the sprites on click

--- UI/main_ui/main_ui.gd
+++ UI/main_ui/main_ui.gd
@@ -3,12 +3,10 @@ enum FRIEND_TYPES {ADDING, MULTIPLYING}
 @onready var hand: Node = $Hand
 @onready var set_up: Node = $SetUp
 
+const MARGIN = 25
 
 # will change as more friends are added to set_up
-var set_up_pos : Vector2 = Vector2(105, 150)
-
-# will change as more cards are added to hand
-var hand_pos : Vector2 = Vector2(120, 475)
+var last_pos : Vector2 = Vector2(-MARGIN, 0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -25,17 +23,24 @@ func generate_hand():
     add_to_hand(FRIEND_TYPES.ADDING)
     add_to_hand(FRIEND_TYPES.ADDING)
     add_to_hand(FRIEND_TYPES.MULTIPLYING)
+    add_to_hand(FRIEND_TYPES.MULTIPLYING)
+    add_to_hand(FRIEND_TYPES.MULTIPLYING)
     add_to_hand(FRIEND_TYPES.ADDING)
 
 
 func add_to_hand(type: FRIEND_TYPES):
     var card_to_add = GameManager.generate_card(type)
-    card_to_add.position = hand_pos
     hand.add_child(card_to_add)
 
 
 func add_to_set_up(type: FRIEND_TYPES):
     var friend_to_add = GameManager.generate_friend(type)
-    friend_to_add.position = set_up_pos
-    set_up_pos.x += 100
+    var sprite_rect = friend_to_add.sprite.get_size()
+    
+    last_pos += Vector2(
+        sprite_rect.x + MARGIN,
+        0
+    ) 
+    
+    friend_to_add.position = last_pos
     set_up.add_child(friend_to_add)
