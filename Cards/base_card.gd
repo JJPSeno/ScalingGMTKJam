504afb43ca3d894b491df5ed3e014ea517f7c233
Accidentallyc <project.cassey@gmail.com>
2024-08-18 18:59:19 +1200

    Not really happy about this, shouldve done tweens instead

--- Cards/base_card.gd
+++ Cards/base_card.gd
@@ -18,6 +18,8 @@ var card_type
 @onready var target_container_size:Vector2 = custom_minimum_size
 @onready var target_scale:Vector2 = transformee.scale
 
+var is_animating_out := false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -35,24 +37,38 @@ func _process(delta: float) -> void:
         
     These values were chosen by hand cause they looked nice
     """
+    var anim_speed = 5 if is_animating_out else 1
     var tgt_size = custom_minimum_size\
-        .move_toward(target_container_size, 5)
+        .move_toward(target_container_size, 5 * anim_speed)
     var tgt_scale = transformee.scale\
-        .move_toward(target_scale, 0.02)
+        .move_toward(target_scale, 0.02 * anim_speed)
     
     custom_minimum_size = tgt_size
     transformee.scale = tgt_scale
+    
+    if is_animating_out \
+        and custom_minimum_size.length() < 1.75 :
+            queue_free()
+        
+    
 func _on_pressed() -> void:
+    if is_animating_out: return
+    
+    is_animating_out = true
+    target_container_size = Vector2.ONE
     GameManager.emit_signal("set_up_card",card_type)
-    queue_free()
 
 func _on_mouse_entered() -> void:
-    var new_size = card_size * Vector2(1.25,1)
+    if is_animating_out: return
+    
+    var new_size = card_size * Vector2(1.25,1.25)
     var new_scale = transformee.scale * Vector2(1.15,1.15)
     target_container_size = new_size
     target_scale = new_scale
 
 
 func _on_mouse_exited() -> void:
+    if is_animating_out: return
+    
     target_container_size = card_size
     target_scale = Vector2.ONE
